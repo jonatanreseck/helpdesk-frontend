a030c7569a47dfd71800b96d7de53435c3f03380
jonatanreseck <jonatanreseck@yahoo.com.br>
2022-08-19 23:19:35 -0300

    aula 37: ng guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path:'', component: NavComponent, children: [
+    path:'', component: NavComponent, canActivate: [AuthGuard], children: [
       {path:'home', component: HomeComponent},
       {path: 'tecnicos', component: TecnicoListComponent }
     ]
