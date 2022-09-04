419db59caa342b78f102d543e55591056a3e6fde
jonatanreseck <jonatanreseck@yahoo.com.br>
2022-09-04 15:35:21 -0300

    AULA 56: Ajuste no HTML do chamado

--- src/app/models/chamado.ts
+++ src/app/models/chamado.ts
@@ -5,7 +5,6 @@ export interface Chamado {
     prioridade: string;
     status: string;
     titulo: string;
-    // descricao: string;
     tecnico: any;
     cliente: any;
     nomeCliente: string;
